de82fd0f2ae609ea809277822e2550e0fe722a89
gurs1kh <gurs1kh@live.com>
2015-12-06 16:01:05 -0800

    tried to add page caching

--- app/src/main/java/edu/washington/singhm5/saveoffline/WebViewerActivity.java
+++ app/src/main/java/edu/washington/singhm5/saveoffline/WebViewerActivity.java
@@ -1,9 +1,12 @@
 package edu.washington.singhm5.saveoffline;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -31,62 +34,21 @@ public class WebViewerActivity extends AppCompatActivity {
 
 
         final WebView webView = (WebView) findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient() {
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+        webView.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
 
-            }
-        });
-        findViewById(R.id.loadWebButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getBaseContext(), UrlLink, Toast.LENGTH_SHORT).show();
-                webView.loadUrl("http://www.google.com");
-            }
-        });
-
-        findViewById(R.id.saveWebButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                webView.saveWebArchive(getFilesDir().getAbsolutePath() + File.separator + "1.xml");
-                Log.i("filepath", getFilesDir().getAbsolutePath() + File.separator + "1.xml");
-            }
-        });
-
-        findViewById(R.id.clearButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                webView.loadUrl("about:blank");
-            }
-        });
-
-        findViewById(R.id.loadLocalButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String filePath = getFilesDir().getAbsolutePath() + File.separator + "1.xml";
-                Log.i("filepath", filePath);
-                String raw_data = null;
-//                try {
-//                    raw_data = FileUtilities.getFileContents(new File(filePath));
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-                raw_data = raw_data.substring(raw_data.indexOf("<html"), raw_data.indexOf("</html") + 7);
-                Log.i("raw_data", raw_data);
-                //webView.loadData(raw_data, "text/html", null);
-                webView.loadDataWithBaseURL("http://google.com/", raw_data, "text/html", "utf-8", null);
-            }
-        });
-    }
-
-    void continueWhenLoaded(WebView webView) {
-        webView.setWebViewClient(new MyWebClient());
-    }
-
-    private class MyWebClient extends WebViewClient {
-        @Override
-        public void onPageFinished(WebView view, String url)
-        {
-
+        if ( !isNetworkAvailable() ) { // loading offline
+            webView.getSettings().setCacheMode( WebSettings.LOAD_CACHE_ELSE_NETWORK );
         }
+        webView.loadUrl(UrlLink);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService( CONNECTIVITY_SERVICE );
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
